9bd558bd9e0f49c733522c68a70061e0ba745593
sharvarianand <sharvaribhondekar23@gmail.com>
2025-12-28 00:12:31 +0530

    Hide Experience Section

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <AboutSection />
       <ProjectsSection />
       <EducationSection />
-      <ExperienceSection />
+      {/* <ExperienceSection /> */}
       <AchievementsSection />
       <ContactSection />
     </>
